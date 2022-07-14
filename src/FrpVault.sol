7aa8366fa944db850ba84433a02353ff5ef256dd
Josip Koncurat <jkoncura@gmail.com>
2022-07-14 18:33:28 +0200

    chore: cleanup

--- src/FrpVault.sol
+++ src/FrpVault.sol
@@ -2,13 +2,14 @@
 
 pragma solidity 0.8.13;
 
-import "openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
-import "openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
-import "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
-import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
-import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
-import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
-import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/access/AccessControlUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/IERC20MetadataUpgradeable.sol";
+import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
 
 import { NotionalViews, MarketParameters } from "./notional/interfaces/INotional.sol";
 import "./notional/interfaces/IWrappedfCashFactory.sol";
@@ -18,6 +19,7 @@ import "./notional/lib/Constants.sol";
 /// @title Fixed rate product vault
 /// @notice Contains logic for integration with Notional
 contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
     using EnumerableSet for EnumerableSet.AddressSet;
 
     /// @notice Responsible for all vault related permissions
@@ -29,7 +31,7 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
     IWrappedfCashFactory public wrappedfCashFactory;
     address public notionalRouter;
 
-    EnumerableSet.AddressSet internal fCashPositions; // This takes 2 slots
+    EnumerableSet.AddressSet fCashPositions; // This takes 2 slots
     uint16 internal slippage;
 
     /// @dev Emitted when minting new FCash during harvest
@@ -65,25 +67,31 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
         wrappedfCashFactory = _wrappedfCashFactory;
         notionalRouter = _notionalRouter;
         slippage = _slippage;
+
+        (address lowestYieldFCash, address highestYieldFCash) = _getLowestHighestMaturity();
+        fCashPositions.add(lowestYieldFCash);
+        fCashPositions.add(highestYieldFCash);
+        IERC20Upgradeable(_asset).approve(highestYieldFCash, type(uint).max);
+        IERC20Upgradeable(_asset).approve(lowestYieldFCash, type(uint).max);
     }
 
     /// @notice Exchanges all the available assets into the highest yielding maturity
     function harvest() external {
-        _redeemAssetsIfMarketMatured();
-
-        uint assetBalance = IERC20Upgradeable(asset()).balanceOf(address(this));
+        bool marketHasMatured = _redeemAssetsIfMarketMatured();
+        // either maxAsset or asset whichever is higher
+        address _asset = asset();
+        uint assetBalance = IERC20Upgradeable(_asset).balanceOf(address(this));
         if (assetBalance == 0) {
             return;
         }
-        uint highestYieldMaturity = _getHighestYieldingMaturity();
+        (address lowestYieldFCash, address highestYieldFCash) = _getLowestHighestMaturity();
+        _sortMaturities(lowestYieldFCash, highestYieldFCash, marketHasMatured);
 
-        IWrappedfCashComplete highestYieldWrappedFCash = IWrappedfCashComplete(
-            wrappedfCashFactory.deployWrapper(currencyId, uint40(highestYieldMaturity))
-        );
-        _cachefCashPosition(address(highestYieldWrappedFCash));
-        uint fCashAmount = _convertAssetsTofCash(assetBalance, highestYieldWrappedFCash);
-        highestYieldWrappedFCash.mintViaUnderlying(assetBalance, uint88(fCashAmount), address(this), 0);
-        emit FCashMinted(highestYieldWrappedFCash, assetBalance, fCashAmount);
+        uint fCashAmount = _convertAssetsTofCash(assetBalance, IWrappedfCashComplete(highestYieldFCash));
+        _safeApprove(_asset, highestYieldFCash, fCashAmount);
+
+        IWrappedfCashComplete(highestYieldFCash).mintViaUnderlying(assetBalance, uint88(fCashAmount), address(this), 0);
+        emit FCashMinted(IWrappedfCashComplete(highestYieldFCash), assetBalance, fCashAmount);
     }
 
     /// @notice Sets slippage
@@ -161,6 +169,7 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
         uint assets,
         uint shares
     ) internal override {
+        // put super._withdraw()
         if (caller != owner) {
             _spendAllowance(owner, caller, shares);
         }
@@ -179,28 +188,21 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
     }
 
     /// @notice Loops through fCash positions and redeems into asset if position has matured
-    function _redeemAssetsIfMarketMatured() internal {
+    function _redeemAssetsIfMarketMatured() internal returns (bool) {
+        // if market has matured returns true which means we need to cache the markets again.
+        bool marketHasMatured;
         uint fCashPositionLength = fCashPositions.length();
         for (uint i = 0; i < fCashPositionLength; i++) {
             IWrappedfCashComplete fCashPosition = IWrappedfCashComplete(fCashPositions.at(i));
             if (fCashPosition.hasMatured()) {
+                marketHasMatured = true;
                 uint fCashAmount = fCashPosition.balanceOf(address(this));
-                fCashPositions.remove(address(fCashPosition));
-                if (fCashAmount == 0) {
-                    continue;
+                if (fCashAmount != 0) {
+                    fCashPosition.redeemToUnderlying(fCashAmount, address(this), type(uint32).max);
                 }
-                fCashPosition.redeemToUnderlying(fCashAmount, address(this), type(uint32).max);
             }
         }
-    }
-
-    /// @notice Caches fCash position
-    /// @param _highestYieldWrappedFCash Address of the wrappedfCash
-    function _cachefCashPosition(address _highestYieldWrappedFCash) internal {
-        if (!fCashPositions.contains(_highestYieldWrappedFCash)) {
-            fCashPositions.add(_highestYieldWrappedFCash);
-            IERC20Upgradeable(asset()).approve(_highestYieldWrappedFCash, type(uint).max);
-        }
+        return marketHasMatured;
     }
 
     /// @notice Withdraws asset from maturities
@@ -209,46 +211,47 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
         address _asset,
         uint _assets,
         uint _maxSlippage
-    ) internal {
+    ) internal virtual {
         if (IERC20Upgradeable(_asset).balanceOf(address(this)) < _assets) {
             // first withdraw from the matured markets.
             _redeemAssetsIfMarketMatured();
-            uint fCashPositionLength = fCashPositions.length();
-            for (uint i = 0; i < fCashPositionLength; i++) {
-                // fetch the asset balance, if it is higher or equal than assets break.
-                // otherwise fetch from the available maturity.
-                uint assetBalance = IERC20Upgradeable(_asset).balanceOf(address(this));
-                if (assetBalance >= _assets) {
-                    break;
-                }
-                IWrappedfCashComplete fCashPosition = IWrappedfCashComplete(fCashPositions.at(i));
-                // For the assets needed to withdraw this is the amount of shares.
-                // 10 is the buffer to avoid reverts since previewWithdraw and actual redeem differ slightly.
-                uint fCashAmountNeeded = fCashPosition.previewWithdraw(_assets + 10 - assetBalance);
-                uint fCashAmountAvailable = fCashPosition.balanceOf(address(this));
-                if (fCashAmountNeeded > fCashAmountAvailable) {
-                    // there isn't enough assets in this position, withdraw all and move to the next maturity
-                    _checkPriceImpactDuringRedemption(
-                        _assets - assetBalance,
-                        fCashAmountAvailable,
-                        fCashPosition,
-                        _maxSlippage
-                    );
-                    fCashPosition.redeemToUnderlying(fCashAmountAvailable, address(this), type(uint32).max);
-                    fCashPositions.remove(address(fCashPosition));
-                } else {
-                    _checkPriceImpactDuringRedemption(0, fCashAmountNeeded, fCashPosition, _maxSlippage);
-                    fCashPosition.redeemToUnderlying(fCashAmountNeeded, address(this), type(uint32).max);
-                    break;
+            uint assetBalance = IERC20Upgradeable(_asset).balanceOf(address(this));
+            if (assetBalance < _assets) {
+                uint amountNeeded = _assets + 1_000 - assetBalance;
+                uint fCashPositionLength = fCashPositions.length();
+                for (uint i = 0; i < fCashPositionLength; i++) {
+                    IWrappedfCashComplete fCashPosition = IWrappedfCashComplete(fCashPositions.at(i));
+
+                    uint fCashAmountNeeded = fCashPosition.previewWithdraw(amountNeeded);
+                    uint fCashAmountAvailable = fCashPosition.balanceOf(address(this));
+
+                    if (fCashAmountAvailable == 0) {
+                        continue;
+                    }
+
+                    if (fCashAmountNeeded > fCashAmountAvailable) {
+                        // there isn't enough assets in this position, withdraw all and move to the next maturity
+                        _checkPriceImpactDuringRedemption(
+                            fCashPosition.previewRedeem(fCashAmountAvailable),
+                            fCashAmountAvailable,
+                            fCashPosition,
+                            _maxSlippage
+                        );
+                        fCashPosition.redeemToUnderlying(fCashAmountAvailable, address(this), type(uint32).max);
+                        amountNeeded = amountNeeded - IERC20Upgradeable(_asset).balanceOf(address(this));
+                    } else {
+                        _checkPriceImpactDuringRedemption(amountNeeded, fCashAmountNeeded, fCashPosition, _maxSlippage);
+                        fCashPosition.redeemToUnderlying(fCashAmountNeeded, address(this), type(uint32).max);
+                        break;
+                    }
                 }
             }
         }
     }
 
     /// @notice Checks for price impact during redemption.
-    /// @dev Passing 0 as _assetAmount acts as function overload to estimate the _assetAmount for _fCashAmount
     /// @param _assetAmount Amount of asset
-    /// @param _fCashAmount Amount of fCash
+    /// @param _fCashAmount Spot amount of fCash
     /// @param _fCashPosition Address of the wrappedfCash
     function _checkPriceImpactDuringRedemption(
         uint _assetAmount,
@@ -256,29 +259,71 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
         IWrappedfCashComplete _fCashPosition,
         uint maxSlippage
     ) internal view {
-        uint shares = _fCashPosition.convertToShares(
-            _assetAmount == 0 ? _fCashPosition.previewRedeem(_fCashAmount) : _assetAmount
-        );
-        require(100_000 - ((shares * 100_000) / _fCashAmount) <= maxSlippage, "FrpVault: PRICE_IMPACT");
+        uint fCashAmountOracle = _fCashPosition.convertToShares(_assetAmount);
+        require(100_000 - ((_fCashAmount * 100_000) / fCashAmountOracle) <= maxSlippage, "FrpVault: PRICE_IMPACT");
     }
 
-    /// @notice Picks the highest yielding maturity from currently active maturities
-    /// @return highestYieldMaturity the highest yielding maturity
-    function _getHighestYieldingMaturity() internal view returns (uint highestYieldMaturity) {
+    function _getLowestHighestMaturity() internal returns (address lowestYieldFCash, address highestYieldFCash) {
         MarketParameters[] memory marketParameters = NotionalViews(notionalRouter).getActiveMarkets(currencyId);
         uint highestOracleRate;
+        uint highestYieldMaturity;
+        uint lowestYieldMaturity;
         for (uint i = 0; i < marketParameters.length; i++) {
             MarketParameters memory parameters = marketParameters[i];
             if (parameters.maturity >= block.timestamp + 2 * Constants.QUARTER) {
                 // it's not 3 or 6 months maturity check the next one
                 continue;
             }
-            uint oracleRate = parameters.oracleRate;
-            if (oracleRate > highestOracleRate) {
-                highestOracleRate = oracleRate;
+            // This sorting algorithm only works in case we are expecting to have two maturities reach this part of the code.
+            if (highestYieldMaturity == 0 && highestOracleRate == 0) {
                 highestYieldMaturity = parameters.maturity;
+                highestOracleRate = parameters.oracleRate;
+            } else {
+                // this is the second iteration now compare and set final values
+                if (highestOracleRate > parameters.oracleRate) {
+                    // initially set oracle rate is higher, just setup the lowestYieldMaturity
+                    lowestYieldMaturity = parameters.maturity;
+                } else {
+                    // swap the value for lowestYieldMaturity and set the highestYieldMaturity to be the current one
+                    lowestYieldMaturity = highestYieldMaturity;
+                    highestYieldMaturity = parameters.maturity;
+                }
             }
-            assert(highestYieldMaturity != 0);
+        }
+        assert(highestYieldMaturity >= lowestYieldMaturity);
+        IWrappedfCashFactory _wrappedfCashFactory = wrappedfCashFactory;
+        uint16 _currencyId = currencyId;
+        highestYieldFCash = _wrappedfCashFactory.deployWrapper(_currencyId, uint40(highestYieldMaturity));
+        lowestYieldFCash = _wrappedfCashFactory.deployWrapper(_currencyId, uint40(lowestYieldMaturity));
+    }
+
+    function _sortMaturities(
+        address lowestYieldFCash,
+        address highestYieldFCash,
+        bool _marketHasMatured
+    ) internal {
+        address first = fCashPositions.at(0);
+        if (_marketHasMatured || first != lowestYieldFCash) {
+            address second = fCashPositions.at(1);
+            fCashPositions.remove(first);
+            fCashPositions.remove(second);
+            fCashPositions.add(lowestYieldFCash);
+            fCashPositions.add(highestYieldFCash);
+        }
+    }
+
+    /// @notice Approves the `_spender` to spend `_requiredAllowance` of `_token`
+    /// @param _token Token address
+    /// @param _spender Spender address
+    /// @param _requiredAllowance Required allowance
+    function _safeApprove(
+        address _token,
+        address _spender,
+        uint _requiredAllowance
+    ) internal {
+        uint allowance = IERC20Upgradeable(_token).allowance(address(this), _spender);
+        if (allowance < _requiredAllowance) {
+            IERC20Upgradeable(_token).safeIncreaseAllowance(_spender, type(uint256).max - allowance);
         }
     }
 
@@ -292,8 +337,8 @@ contract FrpVault is ERC4626Upgradeable, ERC20PermitUpgradeable, AccessControlUp
         returns (uint fCashAmount)
     {
         fCashAmount = _highestYieldWrappedfCash.previewDeposit(_assetBalance);
-        uint assets = _highestYieldWrappedfCash.convertToAssets(fCashAmount);
-        require(100_000 - ((assets * 100_000) / _assetBalance) <= slippage, "FrpVault: PRICE_IMPACT");
+        uint fCashAmountOracle = _highestYieldWrappedfCash.convertToShares(_assetBalance);
+        require(100_000 - ((fCashAmount * 100_000) / fCashAmountOracle) <= slippage, "FrpVault: PRICE_IMPACT");
     }
 
     /// @inheritdoc UUPSUpgradeable
--- test/e2e/frp-vault-notional-interaction.spec.ts
+++ test/e2e/frp-vault-notional-interaction.spec.ts
@@ -3,15 +3,7 @@ import { expect } from 'chai'
 import { BigNumber } from 'ethers'
 import { ethers } from 'hardhat'
 import * as mainnetConfig from '../../eth_mainnet.json'
-import {
-  FrpVault,
-  FrpVaultLowestYieldWithdrawal__factory,
-  FrpVaultMaturity__factory,
-  FrpVault__factory,
-  IERC20,
-  IERC20__factory,
-  WrappedfCashFactory
-} from '../../typechain-types'
+import { FrpVault, FrpVault__factory, IERC20, IERC20__factory, WrappedfCashFactory } from '../../typechain-types'
 import { getStorageAt, impersonate, mineBlocks, reset, setBalance, snapshot } from '../../utils/evm'
 import { expandTo18Decimals, expandTo6Decimals, newProxyContract } from '../../utils/helpers'
 import { deployWrappedfCashFactory, upgradeNotionalProxy } from '../../utils/notional-fixtures'
@@ -53,7 +45,7 @@ describe('FrpVault interaction with wrappedFCash [ @forked-mainnet]', function (
       mainnetConfig.notional.currencyIdUSDC,
       wrappedFCashFactory.address,
       mainnetConfig.notional.router,
-      200
+      2000
     ])
 
     await frpVault.grantRole(VAULT_MANAGER_ROLE, signer.address)
@@ -65,7 +57,7 @@ describe('FrpVault interaction with wrappedFCash [ @forked-mainnet]', function (
   })
 
   beforeEach(async function () {
-    this.timeout(50_000)
+    this.timeout(500_000)
     await snapshot.revert(snapshotId)
   })
 
@@ -94,73 +86,7 @@ describe('FrpVault interaction with wrappedFCash [ @forked-mainnet]', function (
     const usdcAmount = expandTo6Decimals(amount)
     const allowedDeviationPercent = 1
 
-    // usdcWhale deposits USDC
-
-    const shares = await frpVault.previewDeposit(usdcAmount)
-    await frpVault.connect(usdcWhale).deposit(usdcAmount, usdcWhale.address)
-
-    expect(expandTo18Decimals(amount)).to.be.eq(shares)
-    expect(expandTo18Decimals(amount)).to.be.eq(await frpVault.balanceOf(usdcWhale.address))
-
-    // signer deposits usdc
-    const depositTx = await frpVault.deposit(usdcAmount, signer.address)
-    const depositRecipient = await depositTx.wait()
-    console.log('gas used during deposit', depositRecipient.gasUsed.toString())
-
-    expect(expandTo18Decimals(amount)).to.be.eq(await frpVault.balanceOf(signer.address))
-    expect(await frpVault.totalAssets()).to.be.eq(usdcAmount.mul(2))
-
-    const harvestTx = await frpVault.harvest({ gasLimit: 30e6 })
-    const harvestRecipient = await harvestTx.wait()
-    console.log('gas used during harvest', harvestRecipient.gasUsed.toString())
-
-    const usdcBalanceAfterHarvest = await frpVault.balanceOf(await frpVault.asset())
-    await expect(usdcBalanceAfterHarvest).to.eq(0)
-
-    // due to exchange losses shares are worth more
-    const sharesAfterHarvest = await frpVault.previewDeposit(usdcAmount, {
-      gasLimit: 30e6
-    })
-    expect(sharesAfterHarvest.sub(shares)).to.be.gte(
-      BigNumber.from(86_624)
-        .mul(100 - allowedDeviationPercent)
-        .div(100)
-    )
-
-    const totalAssets = await frpVault.totalAssets()
-
-    await mineBlocks(1000)
-
-    // As time passes fCash appreciates in value
-    // TODO: Review why there is some deviation
-    const totalAssetsAfterNBlocks = await frpVault.totalAssets()
-    expect(totalAssetsAfterNBlocks.sub(totalAssets)).to.be.gte(
-      BigNumber.from(763)
-        .mul(100 - allowedDeviationPercent)
-        .div(100)
-    )
-
-    // usdcWhale deposits more USDC
-    await frpVault.connect(usdcWhale).deposit(usdcAmount.mul(1000), usdcWhale.address, { gasLimit: 30e6 })
-
-    const secHarvestTx = await frpVault.harvest({ gasLimit: 30e6 })
-    const secHarvestRecipient = await secHarvestTx.wait()
-    console.log('gas used during sec harvest', secHarvestRecipient.gasUsed.toString())
-
-    await frpVault.connect(usdcWhale).approve(frpVault.address, ethers.constants.MaxUint256)
-    await frpVault
-      .connect(usdcWhale)
-      ['withdraw(uint256,address,address)'](usdcAmount.mul(500), usdcWhale.address, usdcWhale.address, {
-        gasLimit: 30e6
-      })
-  })
-
-  it('harvest/withdrawal flow frpLowestYieldWithdrawal', async () => {
-    const amount = 100
-    const usdcAmount = expandTo6Decimals(amount)
-    const allowedDeviationPercent = 1
-
-    frpVault = await newProxyContract(new FrpVaultLowestYieldWithdrawal__factory(signer), [
+    frpVault = await newProxyContract(new FrpVault__factory(signer), [
       'USDC Notional Vault',
       'USDC_VAULT',
       mainnetConfig.USDC,
@@ -218,108 +144,30 @@ describe('FrpVault interaction with wrappedFCash [ @forked-mainnet]', function (
         .div(100)
     )
 
+    console.log('USDC balance in the frp before deposit:', await USDC.balanceOf(frpVault.address))
+
     // usdcWhale deposits more USDC
-    await frpVault.connect(usdcWhale).deposit(usdcAmount.mul(1000), usdcWhale.address, { gasLimit: 30e6 })
+    await frpVault.connect(usdcWhale).deposit(expandTo6Decimals(1_000_000), usdcWhale.address, { gasLimit: 30e6 })
+
+    console.log('USDC balance in the frp before harvest:', await USDC.balanceOf(frpVault.address))
 
     const secHarvestTx = await frpVault.harvest({ gasLimit: 30e6 })
     const secHarvestRecipient = await secHarvestTx.wait()
     console.log('gas used during sec harvest', secHarvestRecipient.gasUsed.toString())
 
+    console.log('USDC balance in the frp before withdraw:', await USDC.balanceOf(frpVault.address))
+
     await frpVault.connect(usdcWhale).approve(frpVault.address, ethers.constants.MaxUint256)
     const withdrawTx = await frpVault
       .connect(usdcWhale)
-      ['withdraw(uint256,address,address)'](usdcAmount.mul(100), usdcWhale.address, usdcWhale.address, {
+      ['withdraw(uint256,address,address)'](expandTo6Decimals(100_000), usdcWhale.address, usdcWhale.address, {
         gasLimit: 30e6
       })
     const withdrawRecipient = await withdrawTx.wait()
     console.log('gas used during withdrawal', withdrawRecipient.gasUsed.toString())
 
     // There is usdc dust remaining in the vault
-    console.log('USDC balance in the frp:', await USDC.balanceOf(frpVault.address))
-  })
-
-  it('harvest/withdrawal flow frp sorted by maturity', async () => {
-    const amount = 100
-    const usdcAmount = expandTo6Decimals(amount)
-    const allowedDeviationPercent = 1
-
-    frpVault = await newProxyContract(new FrpVaultMaturity__factory(signer), [
-      'USDC Notional Vault',
-      'USDC_VAULT',
-      mainnetConfig.USDC,
-      mainnetConfig.notional.currencyIdUSDC,
-      wrappedFCashFactory.address,
-      mainnetConfig.notional.router,
-      200
-    ])
-    // usdcWhale deposits USDC
-    await USDC.connect(usdcWhale).approve(frpVault.address, ethers.constants.MaxUint256)
-    await USDC.connect(signer).approve(frpVault.address, ethers.constants.MaxUint256)
-
-    const shares = await frpVault.previewDeposit(usdcAmount)
-    console.log('preview deposit gas cost is:', await frpVault.estimateGas.previewDeposit(usdcAmount))
-    console.log('preview withdraw gas cost is:', await frpVault.estimateGas.previewWithdraw(usdcAmount))
-    await frpVault.connect(usdcWhale).deposit(usdcAmount, usdcWhale.address)
-
-    expect(expandTo18Decimals(amount)).to.be.eq(shares)
-    expect(expandTo18Decimals(amount)).to.be.eq(await frpVault.balanceOf(usdcWhale.address))
-
-    // signer deposits usdc
-    const depositTx = await frpVault.deposit(usdcAmount, signer.address)
-    const depositRecipient = await depositTx.wait()
-    console.log('gas used during deposit', depositRecipient.gasUsed.toString())
-
-    expect(expandTo18Decimals(amount)).to.be.eq(await frpVault.balanceOf(signer.address))
-    expect(await frpVault.totalAssets()).to.be.eq(usdcAmount.mul(2))
-
-    const harvestTx = await frpVault.harvest({ gasLimit: 30e6 })
-    const harvestRecipient = await harvestTx.wait()
-    console.log('gas used during harvest', harvestRecipient.gasUsed.toString())
-
-    const usdcBalanceAfterHarvest = await frpVault.balanceOf(await frpVault.asset())
-    await expect(usdcBalanceAfterHarvest).to.eq(0)
-
-    // due to exchange losses shares are worth more
-    const sharesAfterHarvest = await frpVault.previewDeposit(usdcAmount, {
-      gasLimit: 30e6
-    })
-    expect(sharesAfterHarvest.sub(shares)).to.be.gte(
-      BigNumber.from(86_624)
-        .mul(100 - allowedDeviationPercent)
-        .div(100)
-    )
-
-    const totalAssets = await frpVault.totalAssets()
-
-    await mineBlocks(1000)
-
-    // As time passes fCash appreciates in value
-    // TODO: Review why there is some deviation
-    const totalAssetsAfterNBlocks = await frpVault.totalAssets()
-    expect(totalAssetsAfterNBlocks.sub(totalAssets)).to.be.gte(
-      BigNumber.from(763)
-        .mul(100 - allowedDeviationPercent)
-        .div(100)
-    )
-
-    // usdcWhale deposits more USDC
-    await frpVault.connect(usdcWhale).deposit(usdcAmount.mul(1000), usdcWhale.address, { gasLimit: 30e6 })
-
-    const secHarvestTx = await frpVault.harvest({ gasLimit: 30e6 })
-    const secHarvestRecipient = await secHarvestTx.wait()
-    console.log('gas used during sec harvest', secHarvestRecipient.gasUsed.toString())
-
-    await frpVault.connect(usdcWhale).approve(frpVault.address, ethers.constants.MaxUint256)
-    const withdrawTx = await frpVault
-      .connect(usdcWhale)
-      ['withdraw(uint256,address,address)'](usdcAmount.mul(100), usdcWhale.address, usdcWhale.address, {
-        gasLimit: 30e6
-      })
-    const withdrawRecipient = await withdrawTx.wait()
-    console.log('gas used during withdrawal', withdrawRecipient.gasUsed.toString())
-
-    // There is usdc dust remaining in the vault
-    console.log('USDC balance in the frp:', await USDC.balanceOf(frpVault.address))
+    console.log('USDC balance in the frp after withdraw:', await USDC.balanceOf(frpVault.address))
   })
 
   it('fails on harvest with large amounts', async () => {
@@ -334,7 +182,7 @@ describe('FrpVault interaction with wrappedFCash [ @forked-mainnet]', function (
 
     it('successfully', async () => {
       const newSlippage = 50
-      await frpVault.connect(signer).setSlippage(newSlippage)
+      await expect(frpVault.connect(signer).setSlippage(newSlippage)).to.not.be.reverted
       const actualSlippage = await getStorageAt(frpVault, 458)
       await expect(BigNumber.from(actualSlippage)).to.eq(BigNumber.from(newSlippage))
     })
